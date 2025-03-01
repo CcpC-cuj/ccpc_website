ca5699c7cb03469cfed62d6e602ae995bf3e0281
Krish Kumar <imkkrish05@gmail.com>
2025-03-02 00:26:24 +0530

    update

--- src/components/threads/threadshome.js
+++ src/components/threads/threadshome.js
@@ -175,7 +175,38 @@ const ThreadsHome = () => {
   const mobileLayout = (
     <div className="relative min-h-screen bg-black text-white flex flex-col">
       <header className="py-4 text-center border-b border-gray-700">
-        <h1 className="text-2xl font-bold">Threads</h1>
+        <div className="flex items-center justify-center gap-5">
+        <svg width="50" height="50" viewBox="0 0 204 204" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M102 202C157.228 202 202 157.228 202 102C202 46.7715 157.228 2 102 2C46.7715 2 2 46.7715 2 102C2 157.228 46.7715 202 102 202Z" stroke="url(#paint0_linear_0_1)" stroke-width="4"/>
+              <path d="M2 102C68.6667 55.3333 135.333 55.3333 202 102" stroke="url(#paint1_linear_0_1)" stroke-width="3"/>
+              <path d="M102 2C55.3333 68.6667 55.3333 135.333 102 202" stroke="url(#paint2_linear_0_1)" stroke-width="3"/>
+              <path d="M42 42C82 82 122 82 162 42" stroke="url(#paint3_linear_0_1)" stroke-width="3"/>
+              <path d="M102 107C104.761 107 107 104.761 107 102C107 99.2386 104.761 97 102 97C99.2386 97 97 99.2386 97 102C97 104.761 99.2386 107 102 107Z" fill="url(#paint4_linear_0_1)"/>
+              <defs>
+              <linearGradient id="paint0_linear_0_1" x1="2" y1="2" x2="20002" y2="2" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#0077BE"/>
+              <stop offset="1" stop-color="#00C6FF"/>
+              </linearGradient>
+              <linearGradient id="paint1_linear_0_1" x1="2" y1="67" x2="20002" y2="67" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#0077BE"/>
+              <stop offset="1" stop-color="#00C6FF"/>
+              </linearGradient>
+              <linearGradient id="paint2_linear_0_1" x1="67" y1="2" x2="3567" y2="2" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#0077BE"/>
+              <stop offset="1" stop-color="#00C6FF"/>
+              </linearGradient>
+              <linearGradient id="paint3_linear_0_1" x1="42" y1="42" x2="12042" y2="42" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#0077BE"/>
+              <stop offset="1" stop-color="#00C6FF"/>
+              </linearGradient>
+              <linearGradient id="paint4_linear_0_1" x1="97" y1="97" x2="1097" y2="97" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#0077BE"/>
+              <stop offset="1" stop-color="#00C6FF"/>
+              </linearGradient>
+              </defs>
+              </svg>
+              <h1 className="text-2xl font-bold">Threads</h1>
+        </div>
       </header>
       <main className="flex-1 overflow-auto p-2 max-w-xl">{renderPosts()}</main>
       <BottomNavigation onNavigate={handleNavigation} user={user} />
@@ -186,7 +217,38 @@ const ThreadsHome = () => {
   const desktopLayout = (
     <div className="relative min-h-screen bg-black text-white">
       <header className="py-4 border-b border-gray-700 text-center">
-        <h1 className="text-3xl font-bold">Threads</h1>
+      <div className="flex items-center justify-center gap-5">
+        <svg width="50" height="50" viewBox="0 0 204 204" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M102 202C157.228 202 202 157.228 202 102C202 46.7715 157.228 2 102 2C46.7715 2 2 46.7715 2 102C2 157.228 46.7715 202 102 202Z" stroke="url(#paint0_linear_0_1)" stroke-width="4"/>
+              <path d="M2 102C68.6667 55.3333 135.333 55.3333 202 102" stroke="url(#paint1_linear_0_1)" stroke-width="3"/>
+              <path d="M102 2C55.3333 68.6667 55.3333 135.333 102 202" stroke="url(#paint2_linear_0_1)" stroke-width="3"/>
+              <path d="M42 42C82 82 122 82 162 42" stroke="url(#paint3_linear_0_1)" stroke-width="3"/>
+              <path d="M102 107C104.761 107 107 104.761 107 102C107 99.2386 104.761 97 102 97C99.2386 97 97 99.2386 97 102C97 104.761 99.2386 107 102 107Z" fill="url(#paint4_linear_0_1)"/>
+              <defs>
+              <linearGradient id="paint0_linear_0_1" x1="2" y1="2" x2="20002" y2="2" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#0077BE"/>
+              <stop offset="1" stop-color="#00C6FF"/>
+              </linearGradient>
+              <linearGradient id="paint1_linear_0_1" x1="2" y1="67" x2="20002" y2="67" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#0077BE"/>
+              <stop offset="1" stop-color="#00C6FF"/>
+              </linearGradient>
+              <linearGradient id="paint2_linear_0_1" x1="67" y1="2" x2="3567" y2="2" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#0077BE"/>
+              <stop offset="1" stop-color="#00C6FF"/>
+              </linearGradient>
+              <linearGradient id="paint3_linear_0_1" x1="42" y1="42" x2="12042" y2="42" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#0077BE"/>
+              <stop offset="1" stop-color="#00C6FF"/>
+              </linearGradient>
+              <linearGradient id="paint4_linear_0_1" x1="97" y1="97" x2="1097" y2="97" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#0077BE"/>
+              <stop offset="1" stop-color="#00C6FF"/>
+              </linearGradient>
+              </defs>
+              </svg>
+              <h1 className="text-2xl font-bold">Threads</h1>
+        </div>
       </header>
       <div className="relative flex">
         <LeftNavigation onNavigate={handleNavigation} user={user} />
