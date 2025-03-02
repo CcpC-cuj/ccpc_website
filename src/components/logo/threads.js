9bf1f4e56ce5f76a92656bd08a78740f120d35e1
Krish Kumar <imkkrish05@gmail.com>
2025-03-02 07:18:05 +0530

    update

---
+++ src/components/logo/threads.js
@@ -0,0 +1,34 @@
+import * as React from "react";
+const THREADSLOGO = (props) => (
+<svg {...props}
+width="204" height="204" viewBox="0 0 204 204" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M102 202C157.228 202 202 157.228 202 102C202 46.7715 157.228 2 102 2C46.7715 2 2 46.7715 2 102C2 157.228 46.7715 202 102 202Z" stroke="url(#paint0_linear_0_1)" stroke-width="4"/>
+<path d="M2 102C68.6667 55.3333 135.333 55.3333 202 102" stroke="url(#paint1_linear_0_1)" stroke-width="3"/>
+<path d="M102 2C55.3333 68.6667 55.3333 135.333 102 202" stroke="url(#paint2_linear_0_1)" stroke-width="3"/>
+<path d="M42 80C82 120 122 120 162 80" stroke="url(#paint3_linear_0_1)" stroke-width="3"/>
+<path d="M102 107C104.761 107 107 104.761 107 102C107 99.2386 104.761 97 102 97C99.2386 97 97 99.2386 97 102C97 104.761 99.2386 107 102 107Z" fill="url(#paint4_linear_0_1)"/>
+<defs>
+<linearGradient id="paint0_linear_0_1" x1="2" y1="2" x2="20002" y2="2" gradientUnits="userSpaceOnUse">
+<stop stop-color="#0077BE"/>
+<stop offset="1" stop-color="#00C6FF"/>
+</linearGradient>
+<linearGradient id="paint1_linear_0_1" x1="2" y1="67" x2="20002" y2="67" gradientUnits="userSpaceOnUse">
+<stop stop-color="#0077BE"/>
+<stop offset="1" stop-color="#00C6FF"/>
+</linearGradient>
+<linearGradient id="paint2_linear_0_1" x1="67" y1="2" x2="3567" y2="2" gradientUnits="userSpaceOnUse">
+<stop stop-color="#0077BE"/>
+<stop offset="1" stop-color="#00C6FF"/>
+</linearGradient>
+<linearGradient id="paint3_linear_0_1" x1="42" y1="80" x2="12042" y2="80" gradientUnits="userSpaceOnUse">
+<stop stop-color="#0077BE"/>
+<stop offset="1" stop-color="#00C6FF"/>
+</linearGradient>
+<linearGradient id="paint4_linear_0_1" x1="97" y1="97" x2="1097" y2="97" gradientUnits="userSpaceOnUse">
+<stop stop-color="#0077BE"/>
+<stop offset="1" stop-color="#00C6FF"/>
+</linearGradient>
+</defs>
+</svg>
+);
+export default THREADSLOGO;
