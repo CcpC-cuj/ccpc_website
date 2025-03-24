53f90c6e0d746ce56904e69c245437e81e44d89e
Krish Kumar <imkkrish05@gmail.com>
2025-03-24 10:36:56 +0530

    create update

--- src/components/NavbarHome.js
+++ src/components/NavbarHome.js
@@ -168,8 +168,6 @@ const Navbar = () => {
               Reach Us
               <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-blue-500 group-hover:w-full transition-all duration-300"></span>
             </a>
-          </li>
-          <li>
 
           </li>
         </ul>
